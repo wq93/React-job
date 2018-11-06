69c7a1cad664eca3b0fdafd1541f0dcdaffdfe1f
wq93 <214180117@qq.com>
2018-11-06 18:05:22 +0800

    一条命令行启动前台服务及后台服务

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, {Component} from 'react';
 import {Provider} from 'react-redux'
 import {BrowserRouter, Route} from 'react-router-dom';
 import store from './store'
+import axios from 'axios'
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('/users')
+      .then(res => {
+        console.log(res)
+      })
+  }
+
   render() {
     return (
       <Provider store={store}>
