48b3acebf5d8b37c0aae5eed8990a625279d2003
wq93 <214180117@qq.com>
2018-11-09 17:06:20 +0800

    完成登出功能

--- src/component/user/index.js
+++ src/component/user/index.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
-import {Modal, Button, WhiteSpace, WingBlank, Toast} from 'antd-mobile';
-import {connect} from 'react-redux'
-import {actionCreators} from '../../store/user'
+import browserCookie from 'browser-cookies'
 import {Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {Modal, Button, WhiteSpace, WingBlank, Toast} from 'antd-mobile';
+import {actionCreators} from '../../store/user'
 
 const alert = Modal.alert;
 const mapStateToProps = (state) => ({
@@ -28,13 +29,14 @@ class User extends Component {
                 {text: 'Cancel', onPress: () => console.log('cancel')},
                 {
                   text: 'Ok', onPress: () => {
+                    browserCookie.erase('userid')
                     this.props.logoutSubmit()
                   }
                 },
               ])
             }
           >
-            confirm
+            退出登录
           </Button>
         </div>
       ) : <Redirect to={redirectTo}/>
